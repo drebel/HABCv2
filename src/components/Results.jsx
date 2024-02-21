7ad2794f46b6fbe816273eb1c49916ce65190c31
drew <drewbeland@gmail.com>
2024-02-21 15:30:14 -0800

    moved results into a table and added targets

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -27,11 +27,51 @@ export default function Results(props){
 
     return(
         <>
-            <h2>Total Score: {props.calculatedMetrics.totalScore}</h2>
+            <table>
+                <caption>Your Results</caption>
+                <thead>
+                    <tr>
+                        <th scope='col'></th>
+                        <th scope='col'>Your Score</th>
+                        <th scope='col'>Target</th>
+                    </tr>
+                    <tr>
+                        <th scope='row'>Cognitive Subscore</th>
+                        <td>{props.calculatedMetrics.cognitiveScore}</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>Functional Subscore</th>
+                        <td>{props.calculatedMetrics.functionalScore}</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>Behavior and Mood Subscore</th>
+                        <td>{props.calculatedMetrics.behaviorScore}</td>
+                        <td>Less than 2 or 50% reduction within 12 months</td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>Caregiver Stress Subscore</th>
+                        <td>{props.calculatedMetrics.caregiverScore}</td>
+                        <td>Less than 1 or 50% reduction within 12 months</td>
+                    </tr>
+                    <tr>
+                        <th scope='row'>Total</th>
+                        <td>{props.calculatedMetrics.totalScore}</td>
+                        <td>Less than 14 or 50% reduction within 12 months</td>
+                    </tr>
+                </thead>
+            </table>
+
+            {/* <h2>Total Score: {props.calculatedMetrics.totalScore}</h2>
+            <p>Ideal total score: less than 14 or 50% reduction from baseline score by 12 months</p>
             <h3>Cognitive Subscore: {props.calculatedMetrics.cognitiveScore}</h3>
             <h3>Functional Subscore: {props.calculatedMetrics.functionalScore}</h3>
             <h3>Behavior and Mood Subscore: {props.calculatedMetrics.behaviorScore}</h3>
+            <p>Ideal Behavior and Mood score: less than 2 or 50% reduction from baseline score by 12 months</p>
             <h3>Caregiver Stress Subscore: {props.calculatedMetrics.caregiverScore}</h3>
+            <p>Ideal Caregiver Stress score: 
+            less than 1 or 50% reduction from baseline score by 12 months</p> */}
 
             {/* <button onClick={getResults}>console log saved docs by user</button> */}
         </>
