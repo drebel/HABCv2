0d927e2b1f2aed208a3b2c50bd68e79126077c11
drew <drewbeland@gmail.com>
2024-02-29 09:50:15 -0800

    changed radio buttons but checked state not working

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -9,45 +9,53 @@ export default function Question(props){
     return(
         <fieldset key={props.questionKey}>
                     <legend>{props.question}</legend>
-                    <label >
-                        <input 
+                    <label className="btn btn-outline-primary m-1 p-3">
+                        <input
+                            className="btn-check"
                             type="radio"
                             name={props.name}
                             value={0}
                             checked={props.formData[props.name] === 0}
                             onChange={props.handleChange}
                          />
-                        Not At All (0-1 days)
+                        Not At All
+                        <span className="d-block small opacity-90">(0-1 days)</span>
                     </label>
-                    <label >
+                    <label className="btn btn-outline-primary m-1 p-3">
                         <input 
+                            className="btn-check"
                             type="radio"
                             name={props.name}
                             value={1}
                             checked={props.formData[props.name] === 1}
                             onChange={props.handleChange}
                          />
-                        Serveral Days (2-6 days)
+                        Serveral Days 
+                        <span className="d-block small opacity-90">(2-6 days)</span>
                     </label>
-                    <label >
+                    <label className="btn btn-outline-primary m-1 p-3">
                         <input 
+                            className="btn-check"
                             type="radio"
                             name={props.name}
                             value={2}
                             checked={props.formData[props.name] === 2}
                             onChange={props.handleChange}
                          />
-                        More than half the days (7-11 days)
+                        More than half the days
+                        <span className="d-block small opacity-90">(7-11 days)</span>
                     </label>
-                    <label >
-                        <input 
+                    <label className="btn btn-outline-primary m-1 p-3">
+                        <input
+                            className="btn-check" 
                             type="radio"
                             name={props.name}
                             value={3}
                             checked={props.formData[props.name] === 3}
                             onChange={props.handleChange}
                          />
-                        Almost Daily (12-14 days)
+                        Almost Daily
+                        <span className="d-block small opacity-90">(12-14 days)</span> 
                     </label>
                 </fieldset>
     )
