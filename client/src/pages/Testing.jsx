0e792c2c89c46a2f6e9594f30d1f60a796da1e5a
drew <drewbeland@gmail.com>
2024-03-08 14:19:01 -0800

    set up route to get all scores for a user

--- client/src/pages/Testing.jsx
+++ client/src/pages/Testing.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from 'axios'
+import { getAuth } from 'firebase/auth'
 
 // import Hero from '../components/Hero'
 // import Feature1 from '../components/Feature1'
@@ -8,15 +10,30 @@ import React from 'react'
 
 export default function Testing(){
 
+    async function getScores(){
+        // get user id
+        // send the user id in axios get request to server on sepcific route
+        // this is the route XXX
+        // wait to get back an array of docs
+        // console log doc array
+        const auth = getAuth()
+        const user = auth.currentUser
+        console.log(user.uid)
+
+        const res = await axios.get(`http://localhost:5000/score?uid=${user.uid}`)
+        const scoresArray = res.data
+        console.log(scoresArray)
+    }
+
     function handleSubmit(e){
         e.preventDefault()
-        
+        getScores()
     }
 
     return (
         <>
                 <form onSubmit={handleSubmit}>
-                    
+                    <button>get all scores</button>
                 </form>
         </>
     )
--- server/controllers/scoreController.js
+++ server/controllers/scoreController.js
@@ -1,6 +1,19 @@
 const Score = require('../models/Score')
 
 module.exports= {
+    getScores: async (req, res, next) => {
+        // in the req well have the user id
+        // look for all the docs with that user id in the scores collection
+        // return the array with all the docs
+        const uid = req.query.uid
+        try{
+
+            const scoresArray = await Score.find({ createdBy: uid})
+            res.send(scoresArray)
+        }catch(err){
+            res.send({ message: err.message })
+        }
+    },
     postScore: async (req, res, next) => {
         const newScore = new Score(req.body)
         try{
