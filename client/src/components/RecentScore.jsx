3dd7fff1c9d1bfde154591d96deff9e7ed8a04ce
drew <drewbeland@gmail.com>
2024-04-01 18:13:32 -0700

    added a bunch of tweaks to layout of logged out results and dashboard

--- client/src/components/RecentScore.jsx
+++ client/src/components/RecentScore.jsx
@@ -1,29 +1,34 @@
-
+import React from "react"
 
 export default function RecentScore(props){
 
+    // React.useEffect( () => {
+    //     console.log('recentscore component mounting.')
+    //     return () => console.log('recentscore component unmounting.')
+    // },[])
+
     return(
-        <div className='container px-4 py-5'>
+        <div className='container px-4 my-4'>
         <div className='row'>
             <div className='col-12 col-lg d-flex flex-column justify-content-center align-items-center mb-4'>
                 <div>Total Score</div>
-                <div className='display-5'>{props.recentScore.calculatedMetrics.totalScore}</div>
+                <div className='display-5'>{props.calculatedMetrics.totalScore}</div>
             </div>
             <div className='col-12 col-md-6 col-lg d-flex flex-column justify-content-center align-items-center mb-4'>
                 <div>Cognitive Subscore</div>
-                <div className='display-5'>{props.recentScore.calculatedMetrics.cognitiveScore}</div>
+                <div className='display-5'>{props.calculatedMetrics.cognitiveScore}</div>
             </div>
             <div className='col-12 col-md-6 col-lg d-flex flex-column justify-content-center align-items-center mb-4'>
                 <div>Functional Subscore</div>
-                <div className='display-5'>{props.recentScore.calculatedMetrics.functionalScore}</div>
+                <div className='display-5'>{props.calculatedMetrics.functionalScore}</div>
             </div>
             <div className='col-12 col-md-6 col-lg d-flex flex-column justify-content-center align-items-center mb-4'>
                 <div className="">Behavior & Mood Subscore</div>
-                <div className='display-5'>{props.recentScore.calculatedMetrics.behaviorScore}</div>
+                <div className='display-5'>{props.calculatedMetrics.behaviorScore}</div>
             </div>
             <div className='col-12 col-md-6 col-lg d-flex flex-column justify-content-center align-items-center mb-4'>
                 <div>Caregiver Stress Subscore</div>
-                <div className='display-5'>{props.recentScore.calculatedMetrics.caregiverScore}</div>
+                <div className='display-5'>{props.calculatedMetrics.caregiverScore}</div>
             </div>
         </div>
     </div>
